2d0ac7bd34f817abee75b3f700e040293503f2e7
frank <franksavena@gmail.com>
2022-11-16 14:28:09 -0600

    commit tree 48d1608c0c332bb863dac65f11252b5a89dec7c3

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // for(let i = 0; i < array.length; i ++){
+  //   for(let j = i + 1; j < array.length; j++){
+  //     if(array[j] + array[i]  === target) return true
+  //   }
+  // }
+  // return false 
+  
+  const seenNumbers = {}
+  console.log(seenNumbers)
+  for(let i = 0; i < array.length; i++){
+    const compliment = target - array[i]
+    if(seenNumbers[compliment]) return true
+    seenNumbers[array[i]] = true
+  }
+  return false
+
+  // can refactor to for of loop in line 11 for(const number of array)
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  quadratic timing finding the pair that sum is equal to the target integer 
+
+  find compliment either by double iteration or searching key and finding 
+
+  O(n^2) run time - solution 1 
+  O(n) space - solution 1
+
+  O(n) runtime -solution 2 
+  O(n) space - solution 2 
 */
 
 /* 
-  Add your pseudocode here
+ [3,8,12,4,11,7] target sum 10 
+
+ iterate over the array and find two #s that when added equal the target of 10
+
+ 3 + 7 = 10
+
+
 */
 
 /*
